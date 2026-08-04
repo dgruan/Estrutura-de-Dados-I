5a4bad08d4da341fe6109626aa94b1cb6d65a0b0
dgruan <douglasruan27@outlook.com>
2026-08-04 12:56:20 -0300

    Update TwoSums.c

--- TwoSums.c
+++ TwoSums.c
@@ -2,23 +2,24 @@
 
 int main() {
 	
-	int nums[], tam;
+	int tam;
 	long target;
 	
 	scanf("%d", &tam);
-	scanf("%d", &target);
+	scanf("%ld", &target);
+
+    int nums[tam];
 
 	for(int i=0;i<tam;i++){
-		scanf("%d", nums[i]);
+		scanf("%d", &nums[i]);
 	}
 	
 	for(int i=0;i<tam;i++){
 		for(int j=i+1;j<tam;j++){
 			if(nums[i] + nums[j] == target){
 				printf("%d %d",i,j);
-				break;
-		}
+		    }
+	    }
 	}
-	
 	return 0;
 }
